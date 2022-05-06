f64e0567b1bda9465641f05987e431901caac7b5
Jason Jack Tan <52866402+Perthys@users.noreply.github.com>
2022-05-06 15:35:05 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,66 +1,54 @@
-return function(Settings)
-    Settings = Settings or {
-        CheckSynaspeThread = false;
-        CustomSubKeys =  {
-            ":";
-            "%(";
-            "%)";
-        };
-    }
-    
-    if not shared.CustomNameCalls then
-        shared.CustomNameCalls = {} shared.CustomNameCalls.__index = shared.CustomNameCalls
+if shared.CustomNameCalls then
+    return shared.CustomNameCalls 
+end
 
-        local CustomNameCalls = shared.CustomNameCalls;
-        
-        local BlacklistedKeys = Settings["CustomSubKeys"] or {
-            ":";
-            "%(";
-            "%)";
-        }
-        
-        CustomNameCalls.Methods = {
-            GlobalMethods = {
-                
-            }
-        }
-        
-        local Methods = CustomNameCalls.Methods
-        local GlobalMethods = Methods.GlobalMethods;
-        
-        local function CleanString(String)
-            for Index, Value in pairs(BlacklistedKeys) do
-                String = String:gsub(Value, "")
-            end
-            
-            return String
-        end
-        
-        function CustomNameCalls.CreateGlobalMethod(self, Method, Handler)
-            Method = CleanString(Method);
-            Methods.GlobalMethods[Method] = Handler;
-            
-            return function()
-                Methods.GlobalMethods[Method] = nil;
-            end
-        end
-        
-        local OldNameCall; OldNameCall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-            local Method = getnamecallmethod();
-        
-            if GlobalMethods[Method] then
-                if Settings.CheckSynaspeThread and checkcaller()then
-                    return OldNameCall(self, ...)
-                end
-                
-                return GlobalMethods[Method](self, ...)
-            end
-            
-            return OldNameCall(self, ...)
-        end))
-        
-        return shared.CustomNameCalls
-    else
-        return shared.CustomNameCalls
+shared.CustomNameCalls = {}
+shared.CustomNameCalls.__index = shared.CustomNameCalls
+
+local CustomNameCalls = shared.CustomNameCalls
+
+local BlacklistedKeys = {
+    ":",
+    "%(",
+    "%)"
+}
+
+CustomNameCalls.Methods = {
+    GlobalMethods = {}
+}
+
+local Methods = CustomNameCalls.Methods
+local GlobalMethods = Methods.GlobalMethods
+
+local function CleanString(String)
+    for Index, Value in pairs(BlacklistedKeys) do
+        String = String:gsub(Value, "")
+    end
+
+    return String
+end
+
+function CustomNameCalls.CreateGlobalMethod(self, Method, Handler)
+    Method = CleanString(Method)
+    Methods.GlobalMethods[Method] = Handler
+
+    return function()
+        Methods.GlobalMethods[Method] = nil
     end
 end
+
+local OldNameCall; OldNameCall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local Method = getnamecallmethod()
+
+    if GlobalMethods[Method] then
+        if Settings.CheckSynaspeThread and checkcaller() then
+            return OldNameCall(self, ...)
+        end
+        
+        return GlobalMethods[Method](self, ...)
+    end
+    
+    return OldNameCall(self, ...)
+end))
+
+return shared.CustomNameCalls
